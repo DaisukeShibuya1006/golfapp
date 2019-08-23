78575683114af033fdf1925f412a3828a19f8795
DaisukeShibuya1006 <shibudai1006@yahoo.co.jp>
2019-08-23 14:16:53 +0900

    change file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,7 @@ class PostsController < ApplicationController
   before_action :ensure_correct_user, {only:[:edit, :update, :destroy]}
 
   def index
-    @posts = Post.page(params[:page])
-    @posts = Post.page.order(created_at: :desc)
+    @posts = Post.page(params[:page]).order(created_at: :desc)
   end
 
   def show
